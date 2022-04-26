fb2dda01d6f8a6f1a90e0666f3b3471ca5951cdd
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-04-26 18:17:21 +0530

    Time: 12 ms (85.82%), Space: 9.4 MB (93.56%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void fillDFS(vector<vector<int>>& grid, int x, int y){
+        if(x<0 || x==grid.size() || y<0 || y==grid[0].size() || grid[x][y]==1){
+            return;
+        }
+        grid[x][y] = 1;
+        fillDFS(grid, x-1, y);
+        fillDFS(grid, x, y-1);
+        fillDFS(grid, x+1, y);
+        fillDFS(grid, x, y+1);
+    }
+    int closedIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(n==0) return 0;
+        int m = grid[0].size();
+        int cnt = 0;
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<m; ++j){
+                if(grid[i][j]==0){
+                    if(i==0 || j==0 || i==n-1 || j==m-1){
+                        fillDFS(grid, i, j);
+                    }
+                }
+            }
+        }
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<m; ++j){
+                if(grid[i][j]==0){
+                    fillDFS(grid, i, j);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
