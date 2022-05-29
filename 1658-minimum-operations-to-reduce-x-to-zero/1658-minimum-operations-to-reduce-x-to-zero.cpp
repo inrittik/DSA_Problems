01c8b967216b561edd0c434c331049207b0b05ac
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-05-29 17:13:24 +0530

    Time: 181 ms (87.72%), Space: 98.5 MB (73.12%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,45 +2,22 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n=nums.size();
-        unordered_map<int, int> pref;
-        unordered_map<int, int> suff;
+        int total=0;
+        for(int i:nums) total+=i;
+        int target=total-x;
+        int i=0, j=0;
         int sum=0;
-        for(int i=0; i<n; ++i){
-            sum+=nums[i];
-            pref[sum]=i;
-        }
-        if(sum<x) return -1;
-        sum=0;
-        for(int i=n-1; i>=0; --i){
-            sum+=nums[i];
-            suff[sum]=i;
-        }
-        int numOfOp=INT_MAX;
-        for(auto it:pref){
-            int op=0;
-            if(suff.find(x-it.first)!=suff.end()){
-                op=it.second+1;
-                op+=n-suff.find(x-it.first)->second;
-                numOfOp=min(op, numOfOp);
+        int ans=0;
+        while(j<n){
+            sum+=nums[j];
+            while(i<=j && sum>target){
+                sum-=nums[i++];
             }
-            if(x-it.first==0){
-                op=it.second+1;
-                numOfOp=min(op, numOfOp);
+            if(sum==target){
+                ans=max(ans, j-i+1);
             }
+            j++;
         }
-        for(auto it:suff){
-            int op=0;
-            if(pref.find(x-it.first)!=pref.end()){
-                op=n-it.second;
-                op+=pref.find(x-it.first)->second+1;
-                numOfOp=min(op, numOfOp);
-            }
-            if(x-it.first==0){
-                op=n-it.second;
-                numOfOp=min(op, numOfOp);
-            }
-        }
-        if(numOfOp==INT_MAX) return -1;
-        else return numOfOp;
+        return ans == 0 ? (target == 0 ? n : -1) : n - ans;
     }
 };
