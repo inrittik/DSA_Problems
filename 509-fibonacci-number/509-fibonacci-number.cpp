cfa0ef70684434681df5d0aa7f5b674541ccc2ac
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-06-10 20:15:48 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.02%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1) return n;
+        int a = 0;
+        int b = 1;
+        int ans;
+        for(int i=2; i<=n; ++i){
+            ans = a+b;
+            a = b;
+            b = ans;
+        }
+        return ans;
+    }
+};
