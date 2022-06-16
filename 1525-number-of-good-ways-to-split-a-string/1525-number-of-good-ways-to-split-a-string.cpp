fec0e01ab2a2b50405c3fc4b267a28209bf8fa02
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-06-16 13:17:25 +0530

    Time: 341 ms (5.36%), Space: 59.2 MB (5.01%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int numSplits(string s) {
         int n = s.size();
-        vector<int> v1(26);
+        vector<int> v(26);
         unordered_map<int, int> left;
         unordered_map<int, int> right;
         left[-1] = 0;
         for(int i=0; i<n; ++i){
-            if(v1[s[i]-'a']==0) {
+            if(v[s[i]-'a']==0) {
                 left[i] = left[i-1]+1;
-                v1[s[i]-'a']++;
+                v[s[i]-'a']++;
             }
             else left[i] = left[i-1];
         }
         right[n] = 0;
-        vector<int> v2(26);
+        v.clear();
+        v.resize(26);
         for(int i=n-1; i>=0; --i){
-            if(v2[s[i]-'a']==0) {
+            if(v[s[i]-'a']==0) {
                 right[i] = right[i+1]+1;
-                v2[s[i]-'a']++;
+                v[s[i]-'a']++;
             }
             else right[i] = right[i+1];
         }
@@ -26,7 +27,6 @@ public:
         int ans = 0;
         for(int i=0; i<n; ++i){
             if(left[i]==right[i+1]) ans++;
-            // cout<<left[i]<<" "<<right[i]<<endl;
         }
         return ans;
     }
