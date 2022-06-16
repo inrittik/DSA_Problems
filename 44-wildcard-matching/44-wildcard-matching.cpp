6baa06f50ab50d86ffcfd92904a570f381d4ec24
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-06-16 19:13:57 +0530

    Time: 82 ms (53.16%), Space: 28 MB (12.15%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -7,8 +7,8 @@ public:
             if(p[m-1]!='*') return false;
         }
         else if(m==0) return false;
-        if(p[m-1]=='*' && n==0) return dp[n][m]=helper(s, p, n, m-1);
         if(dp[n][m]!=-1) return dp[n][m];
+        if(p[m-1]=='*' && n==0) return dp[n][m]=helper(s, p, n, m-1);
         if(s[n-1]==p[m-1] || p[m-1]=='?') return dp[n][m]=helper(s, p, n-1, m-1);
         else if(p[m-1]=='*') return dp[n][m]=helper(s, p, n-1, m) || helper(s, p, n-1, m-1) || helper(s, p, n, m-1);
         else return dp[n][m]=false;
