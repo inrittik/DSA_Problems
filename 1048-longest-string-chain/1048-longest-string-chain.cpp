055aeeae3e1dbecfc5ad12ea9c4f04393371ecc3
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-06-18 14:22:47 +0530

    Time: 109 ms (71.75%), Space: 17.1 MB (65.13%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -4,19 +4,23 @@ public:
         return a.size()<b.size();
     }
     int longestStrChain(vector<string>& words) {
+        int n = words.size();
         sort(words.begin(), words.end(), comp);
+        vector<int> dp(n, 1);
         unordered_map<string, int> mp;
-        int res=1;
-        for(string s:words){
-            mp[s] = 1;
-            for(int i = 0; i < s.size(); i++) {
-                string prev = s.substr(0, i) + s.substr(i + 1);
-                if (mp.find(prev) != mp.end()) {
-                    mp[s] = max(mp[s], mp[prev] + 1);
-                    res = max(res, mp[s]);
+        int mxLen = 1;
+        for(int i=0; i<n; ++i){
+            mp[words[i]] = 1;
+            int s = words[i].size();
+            for(int c=0; c<s; ++c){
+                string word = words[i].substr(0, c)+words[i].substr(c+1);
+                if(mp.find(word)!=mp.end()){
+                    mp[words[i]] = max(mp[words[i]], mp[word] + 1);
+                    mxLen = max(mxLen, mp[words[i]]);
                 }
             }
         }
-        return res;
+        
+        return mxLen;
     }
 };
