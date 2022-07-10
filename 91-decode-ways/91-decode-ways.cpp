f0502058f8450554a2de31dcfe12ca8aed1d2015
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-07-10 12:05:56 +0530

    Time: 47 ms (5.01%), Space: 8.8 MB (7.26%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int ans = 0;
+    vector<int> dp;
+    void helper(string s, int i, set<string>& st){
+        if(i==s.size()) {
+            ans++;
+            return;
+        }
+        if(dp[i]!=-1) {
+            ans += dp[i];
+            return;
+        }
+        string num = "";
+        for(int k=i; k<s.size(); ++k){
+            num.push_back(s[k]);
+            if(st.find(num)!=st.end()) helper(s, k+1, st);
+        }
+        dp[i] = ans;
+    }
+    int numDecodings(string s) {
+        set<string> st;
+        for(int i=1; i<=26; ++i){
+            st.insert(to_string(i));
+        }
+        dp.resize(s.size(), -1);
+        helper(s, 0, st);
+        return ans;
+    }
+};
