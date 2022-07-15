a9d4a92362e869cff1156cc8b46990b4b862a731
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-07-15 19:38:43 +0530

    Time: 519 ms (19.52%), Space: 153.1 MB (24.72%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    vector<vector<long long>> dp;
-    long long findSum(vector<int>& nums, int n, int picked){
-        if(n<0) return 0;
-        if(dp[n][picked]!=-1) return dp[n][picked];
-        long long taken = findSum(nums, n-1, !picked);
-        long long notTaken = findSum(nums, n-1, picked);
-        if(!picked){
-            return dp[n][picked]=max(notTaken, nums[n]+taken);
-        }
-        else{
-            return dp[n][picked]=max(notTaken, taken-nums[n]);
-        }
-    }
+
     long long maxAlternatingSum(vector<int>& nums) {
-        dp.resize(nums.size(), vector<long long>(2, -1));
-        return findSum(nums, nums.size()-1, 0);
+        int n = nums.size();
+        vector<vector<long long>> dp(n, vector<long long>(2));
+        dp[0][0] = nums[0];
+        for(int i=1; i<n; ++i){
+            dp[i][0]=max(dp[i-1][0], nums[i]+dp[i-1][1]);
+            dp[i][1]=max(dp[i-1][1], dp[i-1][0]-nums[i]);
+        }
+        return max(dp[n-1][0], dp[n-1][1]);
     }
 };
