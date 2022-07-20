f7f926f525a1294ec99dfc9687a6c6cfb65cc417
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-07-20 21:09:05 +0530

    Time: 8 ms (46.85%), Space: 11.6 MB (32.06%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
+    unordered_map<string,int> dp;
     bool helper(string& s1, string& s2, string& s3, int p1, int p2, int p3){
+        string key = to_string(p1)+'*'+to_string(p2)+'*'+to_string(p3);
         if(p3==s3.size()) return true;
-        if(dp[p1][p2][p3]!=-1) return dp[p1][p2][p3];
-        if(s1[p1]==s3[p3] && p1<s1.size() && helper(s1, s2, s3, p1+1, p2, p3+1)) return dp[p1][p2][p3]=true;
-        if(s2[p2]==s3[p3] && p2<s2.size() && helper(s1, s2, s3, p1, p2+1, p3+1)) return dp[p1][p2][p3]=true;
+        if(dp.count(key)) return dp[key];
+        if(s1[p1]==s3[p3] && p1<s1.size() && helper(s1, s2, s3, p1+1, p2, p3+1)) return dp[key]=true;
+        if(s2[p2]==s3[p3] && p2<s2.size() && helper(s1, s2, s3, p1, p2+1, p3+1)) return dp[key]=true;
         
-        return dp[p1][p2][p3]=false;
+        return dp[key]=false;
     }
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size()+s2.size()!=s3.size()) return false;
@@ -23,8 +24,7 @@ public:
             v[c-'a']--;
         }
         if(*max_element(v.begin(), v.end())!=0) return false;
-        
-        dp.resize(s1.size()+1, vector<vector<int>>(s2.size()+1, vector<int>(s3.size()+1, -1)));
+
         return helper(s1, s2, s3, 0, 0, 0);
     }
 };
