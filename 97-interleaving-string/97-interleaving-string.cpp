1e54f4f910b876e3fd5842c3940902c7956afdae
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-07-20 21:20:09 +0530

    Time: 18 ms (31.30%), Space: 13.1 MB (31.00%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -3,8 +3,18 @@ public:
     unordered_map<string,int> dp;
     bool helper(string& s1, string& s2, string& s3, int p1, int p2, int p3){
         string key = to_string(p1)+'*'+to_string(p2)+'*'+to_string(p3);
-        if(p3==s3.size()) return true;
+        if(p3==s3.size()) {
+            return (p1==s1.size() && p2==s2.size())? true:false;
+        }
         if(dp.count(key)) return dp[key];
+        
+        if(p1==s1.size()){
+            return dp[key] = (s2[p2]==s3[p3])?helper(s1, s2, s3, p1, p2+1, p3+1):false;
+        }
+        if(p2==s2.size()){
+            return dp[key] = (s1[p1]==s3[p3])?helper(s1, s2, s3, p1+1, p2, p3+1):false;
+        }
+        
         if(s1[p1]==s3[p3] && p1<s1.size() && helper(s1, s2, s3, p1+1, p2, p3+1)) return dp[key]=true;
         if(s2[p2]==s3[p3] && p2<s2.size() && helper(s1, s2, s3, p1, p2+1, p3+1)) return dp[key]=true;
         
@@ -12,18 +22,6 @@ public:
     }
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size()+s2.size()!=s3.size()) return false;
-        
-        vector<int> v(26);
-        for(char c:s1){
-            v[c-'a']++;
-        }
-        for(char c:s2){
-            v[c-'a']++;
-        }
-        for(char c:s3){
-            v[c-'a']--;
-        }
-        if(*max_element(v.begin(), v.end())!=0) return false;
 
         return helper(s1, s2, s3, 0, 0, 0);
     }
