3ae2ac7adb9e40791d94fb481ca2308df7cea4f8
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-08-26 17:51:18 +0530

    Time: 4 ms (61.58%), Space: 6 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    int getNum(vector<int> num){
-        int n = num.size();
-        int val = 0;
-        for(int i=n-1; i>=0; --i){
-            val = num[i]+val*10;
-        }
-        // cout<<val<<endl;
-        return val;
+    string stringify(int num){
+        string res = to_string(num);
+        sort(res.begin(), res.end());
+        return res;
     }
     bool reorderedPowerOf2(int n) {
-        vector<int> num;
-        if(n<=2) return true;
-        if((n&(n-1))==0) return true;
-        while(n){
-            num.push_back(n%10);
-            n /= 10;
-        }
-        sort(num.begin(), num.end());
-        do{
-            int val = getNum(num);
-            if((val&(val-1))==0 && num[num.size()-1]!=0) return true;
-        }while(next_permutation(num.begin(), num.end()));
-        
+        string str = stringify(n);
+        for(int i=0; i<32; ++i) if(str == stringify(1<<i)) return true;
         return false;
     }
 };
