135a70aab9494fe6eabd9314016a53ed524f3f63
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-10-10 21:22:44 +0530

    Time: 3 ms (45.61%), Space: 6.2 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string breakPalindrome(string p) {
+        string res = "";
+        if(p.size()==1) return res;
+        else{
+            bool flag = false;
+            for(int i=0; i<p.size()/2; ++i){
+                if(p[i]>'a'){
+                    flag = true;
+                    p[i] = 'a';
+                    break;
+                }
+            }
+            if(!flag) p[p.size()-1] += 1;
+            return p;
+        }
+    }
+};
