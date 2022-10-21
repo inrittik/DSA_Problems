d373e5c661acdb67fac83d534eb879c3508da56f
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-10-21 11:08:23 +0530

    Time: 121 ms (15.06%), Space: 12.1 MB (8.94%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if(k>s.length()) return false;
+        unordered_map<char, int> mp;
+        for(char c:s){
+            mp[c]++;
+        }
+        int odd = 0;
+        for(auto it: mp){
+            if(it.second%2!=0) odd++;
+        }
+        return odd<=k;
+    }
+};
