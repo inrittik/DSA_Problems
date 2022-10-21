9a0d3a3b18ebfe2cd94b0559fc3818b46435d360
Nrittik Sarmah <76621681+inrittik@users.noreply.github.com>
2022-10-21 20:25:05 +0530

    Time: 2299 ms (7.36%), Space: 208.1 MB (8.88%) - LeetHub

---
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findOc(string &s, int l, int u){
+        unordered_map<string, int> mp;
+        unordered_map<char, int> cmp;
+        int mx = 0, n = s.size();
+        int i=0, j = 0;
+        while(j<n){
+            cmp[s[j]]++;
+            if((j-i+1)==l && cmp.size()<=u){
+                mx = max(mx, ++mp[s.substr(i, l)]);
+                if(--cmp[s[i]]==0) cmp.erase(s[i]);
+                i++;
+            }
+            else if((j-i+1)==l){
+                if(--cmp[s[i]]==0) cmp.erase(s[i]);
+                i++;
+            }
+            j++;
+        }
+        return mx;
+    }
+    int maxFreq(string s, int mxL, int mnS, int mxS) {
+        int mxOc = 0;
+        for(int i=mnS; i<=mxS; ++i){
+            mxOc = max(mxOc, findOc(s, i, mxL));
+        }
+        return mxOc;
+    }
+};
